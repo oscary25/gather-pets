07f8a9c4f25a26a5dedeecb7f53f9f562ad0b4f2
Oscary Garcia <oscarygarcia@tecnologiateradata15.com>
2020-05-21 21:27:41 -0400

    Create custom hooks

---
+++ src/Components/Hooks/useLocalStorage.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item !== null ? JSON.parse(item) : initialValue;
+    } catch (e) {
+      return initialValue;
+    }
+  });
+
+  const setLocalStorage = (value) => {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value));
+      setStoredValue(value);
+    } catch (e) {
+      console.log("aqui", e);
+    }
+  };
+
+  return [storedValue, setLocalStorage];
+};
+
+export default useLocalStorage;
--- src/Components/PhotoCard/index.js
+++ src/Components/PhotoCard/index.js
@@ -2,21 +2,16 @@ import React, { useEffect, useRef, Fragment, useState } from "react";
 import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 
 import { Article, ImgWrapper, Img, Button } from "./styles";
+import useLocalStorage from "../Hooks/useLocalStorage";
 
 const DEFAULT_IMAGE =
   "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
+
 const PhotoCard = ({ id, like = 0, src = DEFAULT_IMAGE }) => {
   const ref = useRef(null);
   const key = `like-${id}`;
   const [show, setShow] = useState(false);
-  const [liked, setLiked] = useState(() => {
-    try {
-      const like = window.localStorage.getItem(key);
-      return like;
-    } catch (e) {
-      return false;
-    }
-  });
+  const [liked, setLiked] = useLocalStorage(key, false);
   useEffect(() => {
     const observer = new window.IntersectionObserver((entries) => {
       const { isIntersecting } = entries[0];
@@ -30,15 +25,6 @@ const PhotoCard = ({ id, like = 0, src = DEFAULT_IMAGE }) => {
 
   const Icon = liked ? MdFavorite : MdFavoriteBorder;
 
-  const setLocalStorage = (value) => {
-    try {
-      window.localStorage.setItem(key, value);
-      setLiked(value);
-    } catch (e) {
-      console.log("aqui", e);
-    }
-  };
-
   return (
     <div>
       <Article ref={ref}>
@@ -49,7 +35,7 @@ const PhotoCard = ({ id, like = 0, src = DEFAULT_IMAGE }) => {
                 <Img src={src} alt="" />
               </ImgWrapper>
             </a>
-            <Button onClick={() => setLocalStorage(!liked)}>
+            <Button onClick={() => setLiked(!liked)}>
               <Icon size="34px" /> Waaoo {like} likes !
             </Button>
           </Fragment>
