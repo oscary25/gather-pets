9d309d1765b82eebea226c83a1c9d29cf59c6a49
Oscary Garcia <oscarygarcia@tecnologiateradata15.com>
2020-05-24 20:55:14 -0400

    refactor code

--- src/Components/UserForm/index.js
+++ src/Components/UserForm/index.js
@@ -1,12 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
-const useInputValue = (initialValue) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (e) => setValue(e.target.value);
-
-  return { value, onChange };
-};
-
+import useInputValue from "../Hooks/useInputValue";
 const UserForm = ({ onSubmit }) => {
   const value = useInputValue({
     Email: "",
