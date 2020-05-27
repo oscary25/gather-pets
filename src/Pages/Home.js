c439d257251769e6f55e93d7c08e5b578c83164d
Oscary Garcia <oscarygarcia@tecnologiateradata15.com>
2020-05-26 21:24:01 -0400

    Add helmet

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 import ListOfCategories from "../Components/ListOfCategories ";
 import { ListOfPhotoCards } from "../Components/ListOfPhotoCards";
 const Home = ({ id }) => {
   return (
     <div>
+      <Helmet>
+        <title>Petgram- tu app de animales</title>
+        <meta
+          name="description"
+          content="Con petgram puedes encontrar imagen de todo tipo"
+        />
+      </Helmet>
       <ListOfCategories />
       <ListOfPhotoCards categoryId={id} />
     </div>
